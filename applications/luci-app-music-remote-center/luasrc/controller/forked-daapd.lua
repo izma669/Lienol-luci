46dfc5a9ed4e4b2c505c6be1eaa54446b20d66d0
ZXH <42003399+izma669@users.noreply.github.com>
2025-01-15 12:36:58 +0800

    Update forked-daapd.lua

--- applications/luci-app-music-remote-center/luasrc/controller/forked-daapd.lua
+++ applications/luci-app-music-remote-center/luasrc/controller/forked-daapd.lua
@@ -1,20 +1,20 @@
-module("luci.controller.forked-daapd", package.seeall)
+module("luci.controller.owntone", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/forked-daapd") then
+	if not nixio.fs.access("/etc/config/owntone") then
 		return
 	end
 
-	local page = entry({"admin", "nas", "forked-daapd"}, cbi("forked-daapd"), _("Music Remote Center"))
+	local page = entry({"admin", "nas", "owntone"}, cbi("owntone"), _("Music Remote Center"))
 	page.dependent = true
 	page.acl_depends = { "luci-app-music-remote-center" }
 
-	entry({"admin", "nas", "forked-daapd", "status"}, call("act_status")).leaf = true
+	entry({"admin", "nas", "owntone", "status"}, call("act_status")).leaf = true
 end
 
 function act_status()
 	local e = {}
-	e.running = luci.sys.call("pgrep forked-daapd >/dev/null") == 0
+	e.running = luci.sys.call("pgrep owntone >/dev/null") == 0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
